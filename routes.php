d338efed2f03a77d1ff54579baae31751e85a78a
mathewTheGreat <44224652+mathewTheGreat@users.noreply.github.com>
2023-01-12 23:26:16 +0300

    changed folder structure

--- routes.php
+++ routes.php
@@ -2,9 +2,9 @@
 
 require_once __DIR__.'/router.php';
 
-get('/Dairy/cattle', 'Dairy_API/api/getAllCattle.php');
-get('/Dairy/cattle/$id', 'Dairy_API/api/getCattle.php');
-post('/Dairy/cattle', 'Dairy_API/api/createCattle.php');
-post('/Dairy/cattle/$id', 'Dairy_API/api/updateCattle.php');
-delete('/Dairy/cattle/$id', 'Dairy_API/api/deleteCattle.php');
+get('/Dairy/cattle', 'Dairy_API/api/cattle/getAllCattle.php');
+get('/Dairy/cattle/$id', 'Dairy_API/api/cattle/getCattle.php');
+post('/Dairy/cattle', 'Dairy_API/api/cattle/createCattle.php');
+post('/Dairy/cattle/$id', 'Dairy_API/api/cattle/updateCattle.php');
+delete('/Dairy/cattle/$id', 'Dairy_API/api/cattle/deleteCattle.php');
 any('/404','views/404.php');
